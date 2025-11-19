837a241c534b4761f4f2b06d26f9109ba10cb5fc
Seogu Choi <s2000ten@naver.com>
2025-11-19 11:36:51 +0900

    feat: first commit

---
+++ src/main/java/com/my/snacting/SnactingApplication.java
@@ -0,0 +1,13 @@
+package com.my.snacting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnactingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnactingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/snacting/SnactingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.snacting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnactingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
