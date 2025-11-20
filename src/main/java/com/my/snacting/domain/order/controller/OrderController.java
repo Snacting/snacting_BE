f75fc2351909f14834a6ecc5a2468bb2a3efa671
Seogu Choi <s2000ten@naver.com>
2025-11-20 20:55:50 +0900

    feat: 유저 다이렉트 로그인, 유저 주문서 생성 기능 구현

--- src/main/java/com/my/snacting/domain/order/controller/OrderController.java
+++ src/main/java/com/my/snacting/domain/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.my.snacting.domain.order.dto.response.OrderCreateResponse;
 import com.my.snacting.domain.order.service.OrderService;
 import com.my.snacting.global.auth.CurrentUserId;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
+@Tag(name = "Order API", description = "유저 주문서 관련 API")
 public class OrderController {
 
     private final OrderService orderService;
