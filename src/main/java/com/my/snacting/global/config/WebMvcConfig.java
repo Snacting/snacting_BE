6a99877a2ab4e392d980bd23c5f98993f4f40bf9
Seogu Choi <s2000ten@naver.com>
2025-11-22 12:15:54 +0900

    feat: cors 에러 수정

--- src/main/java/com/my/snacting/global/config/WebMvcConfig.java
+++ src/main/java/com/my/snacting/global/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.my.snacting.global.auth.CurrentUserIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserIdArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
